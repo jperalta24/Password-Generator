cd22f38565c838acc57eec520f69d616c515ed8d
Slender James <114044205+jperalta24@users.noreply.github.com>
2022-11-19 22:03:09 -0800

    adds password working password generator

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment Code
 
 //Password arrays
-var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 var alphaLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
@@ -11,38 +11,65 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
  // 1. Prompt user for the password criteria 
- var userInput = window.prompt("Password character length?")
+ var userInput = window.prompt("Password character length? (Choose between 8 -128 characters)");
 
  //converts string into integer
- var passwordLength = parseInt(userInput)
+ var passwordLength = parseInt(userInput);
 
 //returns boolean for userInput
  if (isNaN(passwordLength)) {
-  window.alert("That's not a number!")
-  return
+  window.alert("That's not a number!");
+  return generatePassword();
  } 
 
  // a. password length 8 < 128
- if (passwordLength < 8 || passwordLength > 128) {
-  window.alert("Password length must be between 8 and 128 characters")
-  return
+ else if (passwordLength < 8 || passwordLength > 128) {
+  window.alert("Password length must be between 8 and 128 characters");
+  return generatePassword();
  } 
 
- var userNumbers = window.confirm("Would you like to use numbers in your password?")
- var userSymbols = window.confirm("Would you like to use symbols in your password?")
- var userUpperCase = window.confirm("Would you like to use uppercase letters in your password?")
- var userLowerCase = window.confirm("Would you like to use lowercase letters in your password?")
+ // 2. validate the input.  
+ var userNumbers = window.confirm("Click Ok if you would like to use numbers in your password");
+ var userSymbols = window.confirm("Click Ok if you would like to use special characters in your password");
+ var userUpperCase = window.confirm("Click Ok if you would like to use uppercase letters in your password");
+ var userLowerCase = window.confirm("Click Ok if you would like to use lowercase letters in your password");
 
- git 
- // 2. validate the input. 
- // 3. Generate password.
- console.log (userNumbers);
+ while(userNumbers === false && userSymbols === false && userUpperCase === false && userLowerCase === false) {
+  window.alert ("Please choose at least one parameter");
+  return generatePassword();
+ }
+
+ //creates array for password inputs
+ var passwordPrompts = []
+
+ if (userNumbers) {
+  passwordPrompts = passwordPrompts.concat(numbers);
+ }
+ if (userSymbols) {
+  passwordPrompts = passwordPrompts.concat(symbols);
+ }
+ if (alphaUp) {
+  passwordPrompts = passwordPrompts.concat(alphaUp);
+ }
+ if (alphaLow) {
+  passwordPrompts = passwordPrompts.concat(alphaLow);
+ }
+
+ console.log(passwordPrompts);
+
+// 3. Generate password.
+// var password = "";
+var password = "";
+
+for (let i = 0; i < passwordLength; i++) {
+password = password + passwordPrompts[Math.floor(Math.random() * passwordPrompts.length)];
+}
 // 4. display the generated password on the page 
- return "new password will go here";
+ return password;
 }
 
 // Write password to the #password input
-function writePassword() {
+ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
